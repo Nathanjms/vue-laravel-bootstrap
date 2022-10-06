4d51e828af5a8760d6554e8130e42b295b35b24a
Nathan James <nathanjms@outlook.com>
2022-10-06 21:50:43 +0100

    Min height

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>test</h1>
-<example-component></example-component>
+    <div class="container">
+        <h1>test</h1>
+        <example-component></example-component>
+    </div>
 @endsection
